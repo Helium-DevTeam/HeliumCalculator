59bf49f37f376a1fe08d356709d369848f9e115a
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2022-12-25 21:19:25 +0800

    feat: Initial commit

---
+++ dllmain.cpp
@@ -0,0 +1,37 @@
+﻿#include "Helium.h"
+
+#include <string>
+#include <list>
+#include <any>
+
+#include <exprtk.hpp>
+
+using namespace Helium;
+using namespace std;
+using namespace exprtk;
+
+HELIUM_EXTENSION_EXPORT map<string, string> extension_metadata()
+{
+	return {
+		{"extension_name", "Helium Calculator"},
+		{"author", "Helium DevTeam"},
+		{"url", "https://github.com/Helium-DevTeam/HeliumCalculator"},
+		{"description", "A official calculator extension for Helium."},
+		{"helium_version", ">=0.8.45-alpha.0"}
+	};
+}
+
+int hcalc(string_view event_name, list<any> param)
+{
+	
+}
+
+HELIUM_EXTENSION_EXPORT int on_self_load()
+{
+	return 0;
+}
+
+HELIUM_EXTENSION_EXPORT int on_self_unload()
+{
+	return 0;
+}
